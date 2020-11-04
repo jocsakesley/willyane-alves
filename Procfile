f4cbe4a069888e724568e84a4817a5d9d5635bcb
Jocsã Kesley <jocsadm@gmail.com>
2020-11-04 15:03:42 -0300

    add files requirements and Procfile

---
+++ Procfile
@@ -0,0 +1,1 @@
+gunicorn willyanealves.wsgi --log-file -
--- willyanealves/wsgi.py
+++ willyanealves/wsgi.py
@@ -14,4 +14,4 @@ from dj_static import Cling
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'willyanealves.settings')
 
-application = get_wsgi_application()
+application = Cling(get_wsgi_application())
