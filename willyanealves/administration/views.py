9df9351e4592da254d4cec4c051c313d896b9931
Jocsã Kesley <jocsadm@gmail.com>
2020-12-04 17:05:56 -0300

    Refactoring dashboard

--- willyanealves/administration/views.py
+++ willyanealves/administration/views.py
@@ -1,5 +1,5 @@
 from django.contrib.auth.decorators import login_required
-from django.db.models import Sum, F, FloatField, Count
+from django.db.models import Sum, F, FloatField, Count, ExpressionWrapper
 from django.shortcuts import render
 from willyanealves.customer_service.models import CustomerService
 from .charts import barchart_billing_profit, barchart_customer_service
@@ -7,7 +7,7 @@ from datetime import datetime
 from .forms import DashForm
 
 
-#@login_required(login_url='/accounts/')
+@login_required(login_url='/accounts/')
 def dashboard(request):
     form = DashForm()
     customer_service = CustomerService.objects.prefetch_related('serviceitem')
@@ -16,13 +16,13 @@ def dashboard(request):
         if form.is_valid():
             date_bp = [dbp[0] for dbp in customer_service.filter(date__month=form.cleaned_data['month'], date__year=form.cleaned_data['year']).values_list('date')]
             total = [tb[0] for tb in customer_service.filter(date__month=form.cleaned_data['month'], date__year=form.cleaned_data['year'])\
-                .annotate(total=Sum((F('serviceitem__service__price')*F('serviceitem__quantity'))*(1-(F('serviceitem__customerservice__discount')/100)), output_field=FloatField()))\
+                .annotate(total=ExpressionWrapper(Sum((F('serviceitem__service__price')*F('serviceitem__quantity'))*(1-(F('serviceitem__customerservice__discount')/100))), output_field=FloatField()))\
                 .values_list('total')]
             total_billing = sum(total)
             customer_service_month = customer_service.filter(date__month=form.cleaned_data['month'], date__year=form.cleaned_data['year']).aggregate(Count('id'))['id__count']
             date_cs = [dcs[0] for dcs in customer_service.filter(date__month=form.cleaned_data['month'], date__year=form.cleaned_data['year']).values_list('serviceitem__customerservice__date')]
             profit = [x[0] for x in customer_service.filter(date__month=form.cleaned_data['month'], date__year=form.cleaned_data['year'])\
-                .annotate(total=Sum((F('serviceitem__service__price')*F('serviceitem__quantity'))*(1-(F('serviceitem__customerservice__discount')/100))-F('serviceitem__service__cost'),
+                .annotate(total=ExpressionWrapper(Sum((F('serviceitem__service__price')*F('serviceitem__quantity'))*(1-(F('serviceitem__customerservice__discount')/100))-F('serviceitem__service__cost')),
                                     output_field=FloatField())).values_list('total')]
             total_profit_month = sum(profit)
             service = [s[0] for s in customer_service.filter(date__month=form.cleaned_data['month'], date__year=form.cleaned_data['year']).values_list('serviceitem__service__service')]
@@ -31,12 +31,12 @@ def dashboard(request):
 
     else:
         date_bp = [dbp[0] for dbp in customer_service.filter(date__month=datetime.today().month, date__year=datetime.today().year).values_list('date')]
-        total = [tb[0] for tb in customer_service.filter(date__month=datetime.today().month, date__year=datetime.today().year).annotate(total=Sum((F('serviceitem__service__price') * F('serviceitem__quantity')) * (1 - (F('serviceitem__customerservice__discount') / 100)), output_field=FloatField())).values_list('total')]
+        total = [tb[0] for tb in customer_service.filter(date__month=datetime.today().month, date__year=datetime.today().year).annotate(total=ExpressionWrapper(Sum((F('serviceitem__service__price') * F('serviceitem__quantity')) * (1 - (F('serviceitem__customerservice__discount') / 100))), output_field=FloatField())).values_list('total')]
         total_billing = sum(total)
         customer_service_month = customer_service.filter(date__month=datetime.today().month, date__year=datetime.today().year).aggregate(Count('id'))['id__count']
         date_cs = [dcs[0] for dcs in customer_service.filter(date__month=datetime.today().month, date__year=datetime.today().year).values_list('serviceitem__customerservice__date')]
-        profit = [x[0] for x in customer_service.filter(date__month=datetime.today().month, date__year=datetime.today().year).annotate(total=Sum((F('serviceitem__service__price') * F('serviceitem__quantity')) *
-            (1 - (F('serviceitem__customerservice__discount') / 100)) - F('serviceitem__service__cost'),output_field=FloatField())).values_list('total')]
+        profit = [x[0] for x in customer_service.filter(date__month=datetime.today().month, date__year=datetime.today().year).annotate(total=ExpressionWrapper(Sum((F('serviceitem__service__price') * F('serviceitem__quantity')) *
+            (1 - (F('serviceitem__customerservice__discount') / 100)) - F('serviceitem__service__cost')),output_field=FloatField())).values_list('total')]
         total_profit_month = sum(profit)
         service = [s[0] for s in customer_service.filter(date__month=datetime.today().month, date__year=datetime.today().year).values_list('serviceitem__service__service')]
         qtd = [q[0] for q in customer_service.filter(date__month=datetime.today().month, date__year=datetime.today().year).values_list('serviceitem__quantity')]
