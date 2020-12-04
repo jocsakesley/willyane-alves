9c8b64f3a9efe6aa2cf72913c8c04e6d2abb90c2
Jocsã Kesley <jocsadm@gmail.com>
2020-12-04 16:53:30 -0300

    Refactoring dashboard

--- willyanealves/administration/views.py
+++ willyanealves/administration/views.py
@@ -31,17 +31,15 @@ def dashboard(request):
 
     else:
         date_bp = [dbp[0] for dbp in customer_service.filter(date__month=datetime.today().month, date__year=datetime.today().year).values_list('date')]
-        total = [tb[0] for tb in customer_service.filter(date__month=datetime.today().month, date__year=datetime.today().year) \
-                     .annotate(total=Sum((F('serviceitem__service__price') * F('serviceitem__quantity')) * (1 - (F('serviceitem__customerservice__discount') / 100)), output_field=FloatField()))\
-                     .values_list('total')]
+        total = [tb[0] for tb in customer_service.filter(date__month=datetime.today().month, date__year=datetime.today().year).annotate(total=Sum((F('serviceitem__service__price') * F('serviceitem__quantity')) * (1 - (F('serviceitem__customerservice__discount') / 100)), output_field=FloatField())).values_list('total')]
         total_billing = sum(total)
         customer_service_month = customer_service.filter(date__month=datetime.today().month, date__year=datetime.today().year).aggregate(Count('id'))['id__count']
         date_cs = [dcs[0] for dcs in customer_service.filter(date__month=datetime.today().month, date__year=datetime.today().year).values_list('serviceitem__customerservice__date')]
         profit = [x[0] for x in customer_service.filter(date__month=datetime.today().month, date__year=datetime.today().year).annotate(total=Sum((F('serviceitem__service__price') * F('serviceitem__quantity')) *
             (1 - (F('serviceitem__customerservice__discount') / 100)) - F('serviceitem__service__cost'),output_field=FloatField())).values_list('total')]
         total_profit_month = sum(profit)
-        service = [s[0] for s in customer_service.filter(date__month=form.cleaned_data['month'], date__year=form.cleaned_data['year']).values_list('serviceitem__service__service')]
-        qtd = [q[0] for q in customer_service.filter(date__month=form.cleaned_data['month'], date__year=form.cleaned_data['year']).values_list('serviceitem__quantity')]
+        service = [s[0] for s in customer_service.filter(date__month=datetime.today().month, date__year=datetime.today().year).values_list('serviceitem__service__service')]
+        qtd = [q[0] for q in customer_service.filter(date__month=datetime.today().month, date__year=datetime.today().year).values_list('serviceitem__quantity')]
 
 
     context = {
